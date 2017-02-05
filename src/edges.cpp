b1734e00fab56c321ca73a47311e114893f9524e
Mathew Pregasen <mathexl@gmail.com>
2017-02-05 17:51:07 -0500

    adding primarily scaffolding

--- src/edges.cpp
+++ src/edges.cpp
@@ -3,6 +3,29 @@
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
-  // ADD YOUR CODE HERE
+  int start;
+  int end;
+  int i, j;
+  int vertex1;
+  int vertex2;
+
+  start = 0;
+  end = 1;
+
+  for(i = 0; i < F.rows(); i++){
+    vertex1 = F(i, start);
+    vertex2 = F(i, end);
+    for(j = i + 1; j < F.rows(); j++){ // = to i to avoid repeats
+      if(vertex1 == F(j, 0) || vertex1 == F(j, 1) || vertex1 == F(j, 2)){
+        if(vertex2 == F(j, 0) || vertex2 == F(j, 1) || vertex2 == F(j, 2)){
+          //FOUND!
+          E.resize(E.rows() + 1,2);
+          E(E.rows() - 1, 0) = i;
+          E(E.rows() - 1, 1) = j;
+        }
+      }
+
+    }
+  }
   return E;
 }
