2797438fe7e53b158b6ece2f682d0a321adfa677
Mathew Pregasen <mathexl@gmail.com>
2017-02-05 18:07:43 -0500

    euler characteristic

--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,34 @@
 #include "euler_characteristic.h"
+#include "edges.h"
+#include <iostream>
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi;
-  // ADD YOUR CODE HERE
+  int maxVertex = 0;
+
+  //calculate the number of Vertexes based on the max Vertex Index
+  for(int i; i < F.rows(); i++){
+    if(F(i, 0) > maxVertex){
+      maxVertex = F(i, 0);
+    }
+    if(F(i, 1) > maxVertex){
+      maxVertex = F(i, 2);
+    }
+    if(F(i, 2) > maxVertex){
+      maxVertex = F(i, 2);
+    }
+  }
+
+
+  Eigen::MatrixXi E = edges(F);
+
+  Chi = maxVertex - E.rows() + F.rows();
+
+  std::cout << "Vertices: " << maxVertex << " ::Edges: " << E.rows() << " ::Faces: "
+              << F.rows() << std::endl;
+
+  std::cout << "Euler Characteristic: "
+              << Chi << std::endl;
   return Chi;
 }
