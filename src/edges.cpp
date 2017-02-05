be4aa0703bc546820835084def6fd92c145d880c
Mathew Pregasen <mathexl@gmail.com>
2017-02-05 18:35:50 -0500

    refacotring

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,35 +1,37 @@
 #include "edges.h"
 #include <iostream>
 
-Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
+Eigen::MatrixXi add_the_edges(Eigen::MatrixXi &F, Eigen::MatrixXi &E, int start, int end)
 {
-  Eigen::MatrixXi E;
-  int start;
-  int end;
-  int i, j;
-  int vertex1;
-  int vertex2;
-
-  start = 0;
-  end = 1;
-  for(int p = 0; p < 2; p++){
-    if(end == 2){ start = 1;}
-    else { end = end + 1; }
-
-    for(i = 0; i < F.rows(); i++){
-      vertex1 = F(i, start);
-      vertex2 = F(i, end);
-      for(j = i + 1; j < F.rows(); j++){ // = to i to avoid repeats
-        if(vertex1 == F(j, 0) || vertex1 == F(j, 1) || vertex1 == F(j, 2)){
-          if(vertex2 == F(j, 0) || vertex2 == F(j, 1) || vertex2 == F(j, 2)){
-            E.resize(E.rows() + 1,2);
-            E(E.rows() - 1, 0) = i;
-            E(E.rows() - 1, 1) = j;
-          }
+  for(int i = 0; i < F.rows(); i++){
+    int vertex1 = F(i, start);
+    int vertex2 = F(i, end);
+    for(int j = i + 1; j < F.rows(); j++){ // = to i to avoid repeats
+      if(vertex1 == F(j, 0) || vertex1 == F(j, 1) || vertex1 == F(j, 2)){
+        if(vertex2 == F(j, 0) || vertex2 == F(j, 1) || vertex2 == F(j, 2)){
+          E.resize(E.rows() + 1,2);
+          E(E.rows() - 1, 0) = i;
+          E(E.rows() - 1, 1) = j;
         }
       }
     }
   }
+  return E;
+}
+
+
+Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
+{
+  Eigen::MatrixXi E;
+
+
+  Eigen::MatrixXi X = F;
+
+  E = add_the_edges(X, E, 0, 1);
+  E = add_the_edges(X, E, 1, 2);
+  E = add_the_edges(X, E, 0, 2);
+
+
 
   std::cout << "The matrix E is of size "
             << E.rows() << "x" << E.cols() << std::endl;
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -8,15 +8,15 @@ int euler_characteristic(const Eigen::MatrixXi &F)
   int maxVertex = 0;
 
   //calculate the number of Vertexes based on the max Vertex Index
-  for(int i; i < F.rows(); i++){
-    if(F(i, 0) > maxVertex){
-      maxVertex = F(i, 0);
+  for(int b = 0; b < F.rows(); b++){
+    if(F(b, 0) > maxVertex){
+      maxVertex = F(b, 0);
     }
-    if(F(i, 1) > maxVertex){
-      maxVertex = F(i, 2);
+    if(F(b, 1) > maxVertex){
+      maxVertex = F(b, 2);
     }
-    if(F(i, 2) > maxVertex){
-      maxVertex = F(i, 2);
+    if(F(b, 2) > maxVertex){
+      maxVertex = F(b, 2);
     }
   }
 
